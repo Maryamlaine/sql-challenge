808cfc46e96a87a17a096c3a025ca78ad405d048
Maryam Tabatabaei <59493178+Maryamlaine@users.noreply.github.com>
2020-04-03 12:56:32 -0500

    Update schema.sql

--- Results/schema.sql
+++ Results/schema.sql
@@ -8,7 +8,7 @@
 
 CREATE TABLE departments (
     dept_no VARCHAR   NOT NULL,
-    dept_name string   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
     CONSTRAINT pk_departments PRIMARY KEY (
         dept_no
      )
