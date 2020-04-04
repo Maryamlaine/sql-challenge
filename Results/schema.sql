148465a66a9d8436f115ae4e58e1164da01dab31
Maryam Tabatabaei <59493178+Maryamlaine@users.noreply.github.com>
2020-04-03 19:39:01 -0500

    Update schema.sql

--- Results/schema.sql
+++ Results/schema.sql
@@ -6,75 +6,77 @@
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
-CREATE TABLE departments (
-    dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL,
-    CONSTRAINT pk_departments PRIMARY KEY (
-        dept_no
+CREATE TABLE "dep" (
+    "dep_no" VARCHAR   NOT NULL,
+    "dep_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dep" PRIMARY KEY (
+        "dep_no"
      )
 );
 
-CREATE TABLE dep_employees (
-    emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_data DATE   NOT NULL,
-    CONSTRAINT pk_dep_employees PRIMARY KEY (
-        emp_no
+
+CREATE TABLE "depemp" (
+    "emp_no" INT   NOT NULL,
+    "dep_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_data" DATE   NOT NULL,
+    CONSTRAINT "pk_depemp" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE dep_manager (
-    dept_no VARCHAR   NOT NULL,
-    emp_no INT   NOT NULL,
-    from_data DATE   NOT NULL,
-    to_date DATE   NOT NULL
+CREATE TABLE "depmng" (
+    "dep_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "from_data" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
 );
 
-CREATE TABLE employees (
-    emp_no INT   NOT NULL,
-    birth_date DATE   NOT NULL,
-    first_name VARCHAR   NOT NULL,
-    last_name VARCHAR   NOT NULL,
-    gender VARCHAR   NOT NULL,
-    hire_date DATE   NOT NULL,
-    CONSTRAINT pk_employees PRIMARY KEY (
-        emp_no
+CREATE TABLE "emp" (
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_emp" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE salaries (
-    emp_no INT   NOT NULL,
-    salary INT   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+CREATE TABLE "salary_emp" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
 );
 
-CREATE TABLE titles (
-    emp_no INT   NOT NULL,
-    title VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+CREATE TABLE "title_emp" (
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
 );
 
-ALTER TABLE dep_employees ADD CONSTRAINT fk_dep_employees_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
+ALTER TABLE "depemp" ADD CONSTRAINT "fk_depemp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "emp" ("emp_no");
 
-ALTER TABLE dep_employees ADD CONSTRAINT fk_dep_employees_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
+ALTER TABLE "depemp" ADD CONSTRAINT "fk_depemp_dep_no" FOREIGN KEY("dep_no")
+REFERENCES "dep" ("dep_no");
 
-ALTER TABLE dep_manager ADD CONSTRAINT fk_dep_manager_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
+ALTER TABLE "depmng" ADD CONSTRAINT "fk_depmng_dep_no" FOREIGN KEY("dep_no")
+REFERENCES "dep" ("dep_no");
 
-ALTER TABLE dep_manager ADD CONSTRAINT fk_dep_manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
+ALTER TABLE "depmng" ADD CONSTRAINT "fk_depmng_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "emp" ("emp_no");
 
-ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
+ALTER TABLE "salary_emp" ADD CONSTRAINT "fk_salary_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "emp" ("emp_no");
 
-ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
+ALTER TABLE "title_emp" ADD CONSTRAINT "fk_title_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "emp" ("emp_no");
 
-CREATE INDEX idx_departments_dept_name
-ON departments (dept_name);
+CREATE INDEX "idx_dep_dep_name"
+ON "dep" ("dep_name");
+
 
