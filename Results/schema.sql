566c5fab3a988158d8bca9d02784198f181efaef
Maryam Tabatabaei <m.tab59@gmail.com>
2020-06-27 22:11:14 -0500

    Updated schema.sql

--- Results/schema.sql
+++ Results/schema.sql
@@ -1,11 +1,4 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/kU4kl2
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
 CREATE TABLE "dep" (
     "dep_no" VARCHAR   NOT NULL,
     "dep_name" VARCHAR   NOT NULL,
